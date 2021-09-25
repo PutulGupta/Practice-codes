f053454400807dba54df600ab272a4ee2aa5d57a
PutulGupta <90447393+PutulGupta@users.noreply.github.com>
2021-09-25 12:59:55 +0530

    Add files via upload

--- StudentTest.java
+++ StudentTest.java
@@ -1,28 +1,30 @@
-class Student{
-  // variable declaration
-    int 1id;
-    String name;
-    String gender;
-  //Method Defination
-	boolean updateProfile(String newName)
-	{
-	   name=newName;
-	   return true;
-	}
-}
-class StudentTest{
-  public static void main(String[] args)
-   {  
-	  //creating object of student class
-	    Student s= new Student();
+package eclipsetest;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] studentIds = new int[] {1001, 1002, 1003};
 		
-	  // setting student states
-	   s.1id=1000;
-	   s.name="Putal";
-	   s.gender="Female";
-	   
-	  // updating profile with correct name
-	   s.updateProfile("Putul");
+		StudentTest s1 = new StudentTest(studentIds[0], "Putul", "Female");
+		StudentTest s2 = new StudentTest(studentIds[1], "Shabbu", "Female");
+		StudentTest s3 = new StudentTest(studentIds[0], "Sourav", "Male");
+
+		System.out.println("Name of student1:" + s1.getName());
+		System.out.println("Name of student2:" + s2.getName());
+		System.out.println("Name of student3:" + s3.getName());
+		
+		s1.setName("Shalini");
+		System.out.println("Updated ame of student1:" + s1.getName());
+		
+		System.out.println("Number of student created:" + s.getStudentCount());
+		
+
 	}
-}  	
-	  
+
+	private void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
